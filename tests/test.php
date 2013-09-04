27426c205806a2e687a9320c39b3762ce8a449c3
Christian Mayer <thefox21at@gmail.com>
2013-09-04 17:42:14 +0200

    Tests added.

---
+++ tests/test.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use TheFox\HTML2Markdown\HTML2Markdown;
+
+$html2md = new HTML2Markdown();
+$html2md->test();
+
+$html2md = new HTML2Markdown(file_get_contents(__DIR__.'/blogpost.html'));
+
+
