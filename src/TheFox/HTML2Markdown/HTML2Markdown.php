8dbad31774b3143cd35e4740d0cce4966c983795
Christian Mayer <thefox21at@gmail.com>
2013-09-04 20:05:28 +0200

    Base.

--- src/TheFox/HTML2Markdown/HTML2Markdown.php
+++ src/TheFox/HTML2Markdown/HTML2Markdown.php
@@ -4,17 +4,31 @@ namespace TheFox\HTML2Markdown;
 
 class HTML2Markdown{
 	
+	private $html = '';
 	private $markdown = '';
 	
 	function __construct($str = ''){
-		$dom = new \DOMDocument();
-		if($str){
+		$this->html = $str;
+	}
+	
+	public function parse(){
+		
+		if($this->html){
 			
-			$dom->loadHTML($str);
-			
-			var_export($dom);
+			$dom = new \DOMDocument();
+			if($dom->loadHTML($this->html)){
+				#var_export($dom);
+				
+				foreach($dom->childNodes as $node){
+					print "node: ".get_class($node).": ".$node->nodeName." = '".$node->nodeValue."' (".$node->nodeType.")\n";
+					
+				}
+				
+			}
 			
 		}
+		
+		return $this->getMarkdown();
 	}
 	
 	public function getMarkdown(){
