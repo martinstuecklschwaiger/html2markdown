67c08ad977e907e6046ac74e6b0d0b13e06c949b
Christian Mayer <thefox21at@gmail.com>
2013-09-09 13:23:45 +0200

    Clean up.

--- tests/test.php
+++ tests/test.php
@@ -5,7 +5,5 @@ require_once __DIR__.'/../vendor/autoload.php';
 use TheFox\HTML2Markdown\HTML2Markdown;
 
 $html2md = new HTML2Markdown(file_get_contents(__DIR__.'/blogpost.html'));
-#$html2md = new HTML2Markdown(file_get_contents(__DIR__.'/blogpost2.html'));
-$html2md = new HTML2Markdown(file_get_contents(__DIR__.'/blogpost3.html'));
 
 print "markdown:\n".$html2md->parse()."\n";
